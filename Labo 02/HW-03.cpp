a0dc8c45c6ac1b29f7ec7fa105c9189e026346f6
Alex777 <oalex8393@gmail.com>
2020-01-15 19:13:37 -0600

    Add: accrued salary

---
+++ Labo 02/HW-03.cpp
@@ -0,0 +1,52 @@
+//SUELDO DEVENGADO DE UNA PERSONA EN LOS AÑOS QUE LLEVA LABORANDO
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+struct VendorData{
+	
+	int DUI;
+	string name;
+	int YearHired;
+	int salary;
+	
+};
+
+void Data();
+
+//int AccruedSalary(VendorData AcSalary);
+
+int main(){
+	//declaracion de variables
+	VendorData vendor;
+	int NumberVendor;
+	
+	//pedimos numero de vendedores que ingresara
+	cout << "Numero de vendedores a ingresar: "; cin >> NumberVendor;
+	cin.ignore();
+	
+	//pidiendo datos de los vendedores
+//	do{
+		
+		cout << "\nINGRESE DATOS DE UN VENDEDOR\n" << endl;
+		
+		cout << "Nombre: "; getline(cin, vendor.name);
+		
+		cout << "DUI: "; cin >> vendor.DUI;
+		
+		
+
+		
+//	}while(NumberVendor > 0);
+	
+
+	
+
+
+	
+	cout << vendor.name << endl;
+
+	return 0;	
+}
